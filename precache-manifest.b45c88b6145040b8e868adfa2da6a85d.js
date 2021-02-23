8c9da372e5b171f0069fe540506a51b4df798ee4
Moses <mzmosa99@gmail.com>
2021-02-23 17:25:47 +0100

    Updates

---
+++ precache-manifest.b45c88b6145040b8e868adfa2da6a85d.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = [
+  {
+    "revision": "8ff5634ca2821b5a46aa",
+    "url": "/my_portfolio/static/css/main.c93d354a.chunk.css"
+  },
+  {
+    "revision": "8ff5634ca2821b5a46aa",
+    "url": "/my_portfolio/static/js/main.21028688.chunk.js"
+  },
+  {
+    "revision": "f4e0835a9f15902d0882",
+    "url": "/my_portfolio/static/js/runtime~main.bc1dbe82.js"
+  },
+  {
+    "revision": "cde247c6155bacb61d9f",
+    "url": "/my_portfolio/static/css/2.993a8118.chunk.css"
+  },
+  {
+    "revision": "cde247c6155bacb61d9f",
+    "url": "/my_portfolio/static/js/2.cfa6a9fd.chunk.js"
+  },
+  {
+    "revision": "37e586fb57257d6664d563a2bf3875bf",
+    "url": "/my_portfolio/static/media/kotlin.37e586fb.png"
+  },
+  {
+    "revision": "f1076e02802321918e9e363e1f4071e1",
+    "url": "/my_portfolio/static/media/profile_picture.f1076e02.jpg"
+  },
+  {
+    "revision": "1d040dce44d5228599199041d91bd47f",
+    "url": "/my_portfolio/static/media/project 1.1d040dce.PNG"
+  },
+  {
+    "revision": "42f7c1b3dcf136a18dc09073ccd33447",
+    "url": "/my_portfolio/static/media/project 2.42f7c1b3.PNG"
+  },
+  {
+    "revision": "8a0db5d303403268505fc12284481f6e",
+    "url": "/my_portfolio/static/media/vb (1).8a0db5d3.png"
+  },
+  {
+    "revision": "90381820f0e3fc89bf90a9217a23a7bc",
+    "url": "/my_portfolio/static/media/vb (2).90381820.png"
+  },
+  {
+    "revision": "3ee3190565b02f02d00470bfe368e37d",
+    "url": "/my_portfolio/static/media/vb (3).3ee31905.png"
+  },
+  {
+    "revision": "8f42d5c0927c890d6f9f350e9135da21",
+    "url": "/my_portfolio/static/media/vb (4).8f42d5c0.png"
+  },
+  {
+    "revision": "58e4fd9552cfca14440d9d0e482fa0a5",
+    "url": "/my_portfolio/static/media/rm (1).58e4fd95.png"
+  },
+  {
+    "revision": "482cfa4c3b50f398b335f86c12d14990",
+    "url": "/my_portfolio/static/media/rm (2).482cfa4c.png"
+  },
+  {
+    "revision": "d2adbdd85d2f9bb97162ebd5549e9400",
+    "url": "/my_portfolio/static/media/rm (3).d2adbdd8.png"
+  },
+  {
+    "revision": "1c439bcc7d4d8266684c618edb28458e",
+    "url": "/my_portfolio/static/media/rm (4).1c439bcc.png"
+  },
+  {
+    "revision": "1609fb8368f70632877314fa65197e95",
+    "url": "/my_portfolio/index.html"
+  }
+];
